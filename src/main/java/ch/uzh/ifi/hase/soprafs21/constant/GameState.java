952ece8c55e9eaf68a282a36382cad290e5f5d1b
Julia <julia.kostadinova@hotmail.com>
2021-04-16 12:49:54 +0200

    added enum GameState

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/constant/GameState.java
@@ -0,0 +1,12 @@
+package ch.uzh.ifi.hase.soprafs21.constant;
+
+//TODO see if this is necessary
+//main idea controll the flow of the came by having states that handle the current part of the game.
+
+/**
+ * Enum to determine the current state of the game, and handle events according to the given state.
+ * This is used to trigger certain actions depending on the state.
+ */
+public enum GameState {
+    STARTSETUP,ASSIGNMENT,RECREATING,GUESSING,ENDROUND;
+}
